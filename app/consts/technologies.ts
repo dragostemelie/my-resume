1c750187e66aa5aec172bb476e882b485583d658
Dragos <dragos.temelie@nestorup.com>
2025-05-19 13:06:50 +0000

    Added new technologies;

--- app/consts/technologies.ts
+++ app/consts/technologies.ts
@@ -1,73 +1,88 @@
-import axios from '../assets/technologies/axios.png';
-import docker from '../assets/technologies/docker.png';
-import express from '../assets/technologies/express.png';
-import gsap from '../assets/technologies/gsap.png';
-import markdown from '../assets/technologies/markdown.png';
-import mongo from '../assets/technologies/mongo.png';
-import msgraph from '../assets/technologies/ms-graph.png';
-import mui from '../assets/technologies/mui.png';
-import next from '../assets/technologies/next.png';
-import postgres from '../assets/technologies/postgres.png';
-import react from '../assets/technologies/react.png';
-import redux from '../assets/technologies/redux.png';
-import styled from '../assets/technologies/styled.png';
-import typescript from '../assets/technologies/typescript.png';
+import axios from "../assets/technologies/axios.png";
+import docker from "../assets/technologies/docker.png";
+import express from "../assets/technologies/express.png";
+import gsap from "../assets/technologies/gsap.png";
+import markdown from "../assets/technologies/markdown.png";
+import mongo from "../assets/technologies/mongo.png";
+import msgraph from "../assets/technologies/ms-graph.png";
+import mui from "../assets/technologies/mui.png";
+import next from "../assets/technologies/next.png";
+import postgres from "../assets/technologies/postgres.png";
+import react from "../assets/technologies/react.png";
+import redux from "../assets/technologies/redux.png";
+import styled from "../assets/technologies/styled.png";
+import typescript from "../assets/technologies/typescript.png";
+import storybook from "../assets/technologies/storybook.png";
+import mysql from "../assets/technologies/mysql.png";
+import bootstrap from "../assets/technologies/bootstrap.png";
 
 export const TECH = {
-  axios: {
-    image: axios,
-    name: 'Axios',
-  },
-  docker: {
-    image: docker,
-    name: 'Docker',
-  },
-  express: {
-    image: express,
-    name: 'Express',
-  },
-  gsap: {
-    image: gsap,
-    name: 'Green Sock',
-  },
-  markdown: {
-    image: markdown,
-    name: 'Markdown',
-  },
-  mongo: {
-    image: mongo,
-    name: 'Mongo DB',
-  },
-  msgraph: {
-    image: msgraph,
-    name: 'Ms Graph',
-  },
-  mui: {
-    image: mui,
-    name: 'Material UI',
-  },
-  next: {
-    image: next,
-    name: 'Next JS',
-  },
-  postgres: {
-    image: postgres,
-    name: 'Postgres',
-  },
-  react: {
-    image: react,
-    name: 'React',
-  },
-  redux: {
-    image: redux,
-    name: 'Redux',
-  },
-  styled: {
-    image: styled,
-    name: 'Styled Components',
-  },
-  typescript: {
-    image: typescript,
-    name: 'TypeScript',
-  },
+    axios: {
+        image: axios,
+        name: "Axios",
+    },
+    docker: {
+        image: docker,
+        name: "Docker",
+    },
+    express: {
+        image: express,
+        name: "Express",
+    },
+    gsap: {
+        image: gsap,
+        name: "Green Sock",
+    },
+    markdown: {
+        image: markdown,
+        name: "Markdown",
+    },
+    mongo: {
+        image: mongo,
+        name: "Mongo DB",
+    },
+    msgraph: {
+        image: msgraph,
+        name: "Ms Graph",
+    },
+    mui: {
+        image: mui,
+        name: "Material UI",
+    },
+    next: {
+        image: next,
+        name: "Next JS",
+    },
+    postgres: {
+        image: postgres,
+        name: "Postgres",
+    },
+    react: {
+        image: react,
+        name: "React",
+    },
+    redux: {
+        image: redux,
+        name: "Redux",
+    },
+    styled: {
+        image: styled,
+        name: "Styled Components",
+    },
+    typescript: {
+        image: typescript,
+        name: "TypeScript",
+    },
+    storybook: {
+        image: storybook,
+        name: "Storybook",
+    },
+    mysql: {
+        image: mysql,
+        name: "MySQL",
+    },
+    bootstrap: {
+        image: bootstrap,
+        name: "Bootstrap",
+    },
 };
